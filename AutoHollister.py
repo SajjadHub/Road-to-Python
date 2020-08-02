c6044392f62bf093ffba621908772e5544b8f5b2
SajjadHub <alkazzazsajjad@gmail.com>
2020-08-02 12:06:46 -0700

    Basic AutoHollister

--- AutoHollister.py
+++ AutoHollister.py
@@ -7,15 +7,19 @@ URL = "https://my.anfcorp.com/psp/hr92pxt/?cmd=login"
 schedule = "https://pshrprdext.anfcorp.com/psc/hr92pxt/EXTERNAL/HRMSEXT/c/NUI_FRAMEWORK.PT_AGSTARTPAGE_NUI.GBL?CONTEXTIDPARAMS=TEMPLATE_ID%3aPTPPNAVCOL&scname=AFHR_AF_STORE_TA1_FL&PanelCollapsible=Y&PTPPB_GROUPLET_ID=AF_STORE_TA1_FL&CRefName=AFHR_STORE_FL1"
 
 data = {
+        "timezoneOffset": "420",
+        "ptmode": "f",
+        "ptlangcd": "ENG",
+        "ptinstalledlang": "GER,ENG,FRA,JPN,ZHS,POL,ITA,KOR,ESP,DUT",
         "userid": "02315004",
         "pwd": "2907981733"
         }
 
-payload = {'data': json.dumps(data)}
+# payload = {'data': json.dumps(data)}
 with requests.Session() as session:
-    post = session.post(URL, data=payload)
+    post = session.post(URL, data=data)
     page = session.get(schedule)
-    print(page.text)
+    # print(page.text)
     soup = BeautifulSoup(page.text, 'html.parser')
     rows = soup.find_all('tr')
     for row in rows:
